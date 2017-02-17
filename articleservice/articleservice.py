e01ab43bf016da2d830f2d122cd7bedd3075c25b
Christoffer Persson <christoffer@fripost.org>
2017-02-17 14:26:16 +0100

    services: commandline set port

--- articleservice/articleservice.py
+++ articleservice/articleservice.py
@@ -1,3 +1,5 @@
+import sys
+
 from flask import Flask, Blueprint
 app = Flask(__name__)
 bp = Blueprint('articles', __name__, url_prefix='/articles')
@@ -8,5 +10,9 @@ def list():
     return "Here are all my articles"
 
 if __name__ == "__main__":
+    port = 6000
+    if len(sys.argv) >= 2:
+        port = sys.argv[1]
+
     app.register_blueprint(bp)
-    app.run()
+    app.run(port=port)
--- commentservice/commentservice.py
+++ commentservice/commentservice.py
@@ -1,3 +1,5 @@
+import sys
+
 from flask import Flask, Blueprint
 app = Flask(__name__)
 bp = Blueprint('comments', __name__, url_prefix='/comments')
@@ -8,5 +10,9 @@ def list():
     return "Here are all my comments"
 
 if __name__ == "__main__":
+    port = 6001
+    if len(sys.argv) >= 2:
+        port = sys.argv[1]
+
     app.register_blueprint(bp)
-    app.run()
+    app.run(port=port)
