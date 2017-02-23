add83cdab32e3146042e0c0f6573a16dcad6f945
Christoffer Persson <christoffer@fripost.org>
2017-02-23 14:34:38 +0100

    apigateway: added some comments

--- apigateway/apigateway.py
+++ apigateway/apigateway.py
@@ -8,18 +8,23 @@ bp = Blueprint('apigateway', __name__, url_prefix='/api')
 @bp.route("/<service_id>", defaults={'path': ''})
 @bp.route("/<service_id>/<path:path>")
 def route(service_id, path):
+
+    # Get the service from the service registry
     discoveryservice_url = 'http://' + '/'.join([app.config['DISCOVERY_SERVICE_URL'], service_id])
     discoveryservice_response = requests.get(discoveryservice_url)
 
+    # Build the service url
     service = discoveryservice_response.json()
     service_url = service['host'] + ':' + str(service['port'])
     service_request_url = 'http://' + '/'.join([service_url, service_id, path])
 
+    # Log some stuff
     print('route:', service_id, path)
     print('json:', request.get_json())
     print('method:', request.method)
     print('service_url:', service_request_url)
 
+    # Forward the request to the service
     service_response = None
     if request.method == 'GET':
         service_response = requests.get(service_request_url, json = request.get_json())
