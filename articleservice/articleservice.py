6b8e79b17c0f91640b3304c40d95f11c7e438fd3
Christoffer Persson <christoffer@fripost.org>
2017-02-23 15:02:12 +0100

    articles: init service

--- articleservice/articleservice.py
+++ articleservice/articleservice.py
@@ -1,19 +1,34 @@
 import sys
+import requests
 
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, jsonify
 app = Flask(__name__)
 bp = Blueprint('articles', __name__, url_prefix='/articles')
 
 
 @bp.route("/", methods=["GET"])
 def list():
-    return "Here are all my articles"
+    return jsonify("Here are all my articles")
 
 if __name__ == "__main__":
     port = 6000
+
+    # Read port from commandline
     if len(sys.argv) >= 2:
         port = sys.argv[1]
 
+    # Setup app
     app.register_blueprint(bp)
     app.config.from_object('settings')
+
+    # Register to discovery service
+    discoveryServiceRequest = 'http://' + app.config['DISCOVERY_SERVICE_URL'] + '/' + bp.name
+    discoveryServiceData = {
+        'service_id': bp.name,
+        'host': 'localhost',
+        'port': port
+        }
+    response = requests.put(discoveryServiceRequest, json = discoveryServiceData)
+
+    # Start app
     app.run(port=port)
