08e618e861d0a7be5f930a8f0ded83ec3c57fb7c
Christoffer Persson <christoffer@fripost.org>
2017-03-18 12:20:26 +0100

    add microAPI.js
    This is a client for the backend

---
+++ frontend/js/MicroApi.js
@@ -0,0 +1,32 @@
+DEFAULT_HOST = '127.0.0.1';
+DEFAULT_PORT = '6101'
+
+function MicroApi(host, port) {
+
+    this.host = host | DEFAULT_HOST;
+    this.port = port | DEFAULT_PORT;
+
+    this.url = 'http://' + this.host + ':' + this.port;
+}
+
+MicroApi.prototype.listArticles = function(successCallback, errorCallback) {
+    var endpoint = this.url + '/api/articles';
+    $.ajax({
+        url: endpoint,
+        type: 'GET',
+        success: successCallback,
+        error: errorCallback
+    });
+};
+
+MicroApi.prototype.getArticle = function(articleId) {
+
+};
+
+MicroApi.prototype.addArticle = function(title, content) {
+
+};
+
+MicroApi.prototype.addComment = function(articleId, message) {
+
+};
