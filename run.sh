026651bcc92dbc12e5fb527e102f57fcb2fee052
Christoffer Persson <christoffer.g.persson@gmail.com>
2017-05-10 08:51:24 +0200

    run-script: add short sleep when initializing the setup

--- run.sh
+++ run.sh
@@ -12,14 +12,11 @@ tmux split-window -d -t 3 -v
 tmux send-keys -t 0 'python discoveryservice/discoveryservice.py' enter
 tmux send-keys -t 2 'python apigateway/apigateway.py' enter
 
+sleep 1
+
 tmux send-keys -t 4 'python articleservice/articleservice.py' enter
 tmux send-keys -t 1 'python commentservice/commentservice.py' enter
 
 tmux send-keys -t 3 'cd frontend; python -m http.server 8000' enter
-
-tmux send-keys -t 1 'echo 1' enter
-tmux send-keys -t 2 'echo 2' enter
-tmux send-keys -t 3 'echo 3' enter
-tmux send-keys -t 4 'echo 4' enter
 
 tmux attach
